c8afc86066c8d907784e35de36085068ef5a3b18
Amreet711 <sunny1935@gmail.com>
2026-06-27 18:25:52 +1000

    Alphabet Recogniser

---
+++ And/Alphabet Recogniser.py
@@ -0,0 +1,55 @@
+letter=input("Enter a character:")
+if input == 'a':
+    print(letter,"is a letter of the alphabet")
+elif input == 'b': 
+    print(letter,"is a letter of the alphabet")
+elif input == 'c': 
+    print(letter,"is a letter of the alphabet")
+elif input == 'd': 
+    print(letter,"is a letter of the alphabet")
+elif input == 'e': 
+    print(letter,"is a letter of the alphabet")
+elif input == 'f': 
+    print(letter,"is a letter of the alphabet")
+elif input =='g': 
+    print(letter,"is a letter of the alphabet")
+elif input =='h': 
+    print(letter,"is a letter of the alphabet")
+elif input =='i': 
+    print(letter,"is a letter of the alphabet")
+elif input =='j': 
+    print(letter,"is a letter of the alphabet")
+elif input =='k': 
+    print(letter,"is a letter of the alphabet")
+elif input =='l': 
+    print(letter,"is a letter of the alphabet")
+elif input =='m': 
+    print(letter,"is a letter of the alphabet")
+elif input =='n': 
+    print(letter,"is a letter of the alphabet")
+elif input =='o': 
+    print(letter,"is a letter of the alphabet")
+elif input =='p': 
+    print(letter,"is a letter of the alphabet")
+elif input =='q': 
+    print(letter,"is a letter of the alphabet")
+elif input =='r': 
+    print(letter,"is a letter of the alphabet")
+elif input =='s': 
+    print(letter,"is a letter of the alphabet")
+elif input =='t': 
+    print(letter,"is a letter of the alphabet")
+elif input =='u': 
+    print(letter,"is a letter of the alphabet")
+elif input =='v': 
+    print(letter,"is a letter of the alphabet")
+elif input =='w': 
+    print(letter,"is a letter of the alphabet")
+elif input =='x': 
+    print(letter,"is a letter of the alphabet")
+elif input =='y': 
+    print(letter,"is a letter of the alphabet")
+elif input =='z': 
+    print(letter,"is a letter of the alphabet")
+else:
+    print(letter,"is not a letter of the alphabet")
